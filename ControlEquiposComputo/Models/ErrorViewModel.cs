213e84e5927aa106262965b5b03f514d28779b31
tiggui <acaruajulcat22_2@unc.edu.pe>
2024-09-18 09:56:59 -0500

    Agregar archivos de proyecto.

---
+++ ControlEquiposComputo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControlEquiposComputo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
