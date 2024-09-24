f09952de43fdf8d461c91716ee8f65e930d82a89
tiggui <acaruajulcat22_2@unc.edu.pe>
2024-09-24 08:01:02 -0500

    Home Panel

--- ControlEquiposComputo/Controllers/HomeController.cs
+++ ControlEquiposComputo/Controllers/HomeController.cs
@@ -1,32 +1,59 @@
+using ControlEquiposComputo.Data;
 using ControlEquiposComputo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ControlEquiposComputo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext db;
+
+        public HomeController(AppDbContext context)
         {
-            _logger = logger;
+            db = context;
         }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
+            
+            var laboratorios = db.Laboratorios.ToList();
+            var equipos = db.Equipos.ToList();
+            var mantenimientosPendientes = db.Mantenimientos.Where(m => m.Descripcion != "Pendiente").ToList();
+            var reservasHoy = db.Reservas.Where(r => r.FechaReserva == System.DateTime.Today).ToList();
+            var usoHoy = db.Reservas.Where(u => u.FechaReserva == System.DateTime.Today).ToList();
+
+            
+            ViewBag.Laboratorios = laboratorios;
+            ViewBag.Equipos = equipos;
+            ViewBag.MantenimientosPendientes = mantenimientosPendientes;
+            ViewBag.ReservasHoy = reservasHoy;
+            ViewBag.UsoHoy = usoHoy;
+
             return View();
-        }
+            //private readonly ILogger<HomeController> _logger;
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            //public HomeController(ILogger<HomeController> logger)
+            //{
+            //    _logger = logger;
+            //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //public IActionResult Index()
+            //{
+            //    return View();
+            //}
+
+            //public IActionResult Privacy()
+            //{
+            //    return View();
+            //}
+
+            //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            //public IActionResult Error()
+            //{
+            //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //}
         }
     }
 }
